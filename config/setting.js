bb23462582e6a4082db34967761c114960628ebb
lzhxwmr <lzhxwmr@gmail.com>
2020-06-30 23:31:10 -0700

    remove credentials

--- config/setting.js
+++ config/setting.js
@@ -1,7 +1,7 @@
 const debug = false;        // whether to open console log debug
-const wordDicPath = "./config/dic.json";    // path for your word dictionary
+const wordDicPath = "./config/sample.json";    // path for your word dictionary
 
-const token = 'NzI3NzE5NzU3Njk2MDczNzY4.Xvv8Yg.rFn6twtZuj-8lWpBc9lNQOHKvlA'; // token for your bot
+const token = 'your tokens'; // token for your bot
 
 module.exports = {
     debug,
