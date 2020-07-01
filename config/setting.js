f79936b508dc6aeb1f1cb3394ccd2e4006fdc4a8
Zhiheng Li <lzhxwmr@gmail.com>
2020-07-01 00:28:02 -0700

    Create setting.js

---
+++ config/setting.js
@@ -0,0 +1,10 @@
+const debug = false;        // whether to open console log debug
+const wordDicPath = "./config/sample.json";    // path for your word dictionary
+
+const token = 'your tokens'; // token for your bot
+
+module.exports = {
+    debug,
+    wordDicPath,
+    token
+}
